d7df15831cf578321415d88bbeabd5e9ab6b4764
nullvoid kanyok <gunyok.official@gmail.com>
2024-02-24 15:21:30 +0700

    style : cyber neon light

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // script.js
 
 async function fetchTemperature() {
-    const response = await fetch('http://127.0.0.1:8000/status' , { method: 'GET' });
+    const response = await fetch("http://127.0.0.1:8000/status", {
+        method: "GET",
+    });
     const data = await response.json();
     return data;
 }
@@ -9,73 +11,76 @@ async function updateTemperature() {
     const data = await fetchTemperature();
     console.log("Fetched data:", data);
 
-    const statusElement = document.getElementById('is_hanging');
-    statusElement.textContent = 'Loading...';
+    const statusElement = document.getElementById("is_hanging");
+    statusElement.textContent = "Loading...";
     statusElement.textContent = `Status : ${data.is_hanging}`;
 
-    const actionButton = document.getElementById('actionButton');
+    const actionButton = document.getElementById("actionButton");
     if (data.is_hanging) {
-        actionButton.textContent = 'Collect Cloth In';
+        actionButton.textContent = "Collect Cloth In";
     } else {
-        actionButton.textContent = 'Hang Cloth Out';
+        actionButton.textContent = "Hang Cloth Out";
     }
 
-    const autoButton = document.getElementById('autoButton');
+    const autoButton = document.getElementById("autoButton");
     if (data.is_auto) {
-        autoButton.textContent = 'System: Auto';
+        autoButton.textContent = "System: Auto";
     } else {
-        autoButton.textContent = 'System: Manual';
+        autoButton.textContent = "System: Manual";
     }
 
-    const temperatureElement = document.getElementById('temperature');
-    temperatureElement.textContent = 'Loading...';
-    temperatureElement.textContent = `temperature: ${data.temperature} °C`;
+    const temperatureElement = document.getElementById("temperature");
+    temperatureElement.textContent = "Loading...";
+    temperatureElement.textContent = `${data.temperature}`;
 
-    const humidityElement = document.getElementById('humidity');
-    humidityElement.textContent = 'Loading...';
-    humidityElement.textContent = `humidity: ${data.humidity}`;
+    const humidityElement = document.getElementById("humidity");
+    humidityElement.textContent = "Loading...";
+    humidityElement.textContent = `${data.humidity}`;
 
-    const lightElement = document.getElementById('light');
-    lightElement.textContent = 'Loading...';
-    lightElement.textContent = `light: ${data.light} °C`;
+    const lightElement = document.getElementById("light");
+    lightElement.textContent = "Loading...";
+    lightElement.textContent = `${data.light}`;
 
-    const rainElement = document.getElementById('rain');
-    rainElement.textContent = 'Loading...';
-    rainElement.textContent = `rain: ${data.rain} °C`;
-
+    const rainElement = document.getElementById("rain");
+    rainElement.textContent = "Loading...";
+    rainElement.textContent = `${data.rain}`;
 }
 async function forceCollect() {
     try {
-        const response = await fetch('http://127.0.0.1:8000/force-collect', { method: 'GET' });
+        const response = await fetch("http://127.0.0.1:8000/force-collect", {
+            method: "GET",
+        });
         if (response.ok) {
-            alert('Action executed successfully');
+            alert("Action executed successfully");
             updateTemperature();
         } else {
-            throw new Error('Failed to execute action');
+            throw new Error("Failed to execute action");
         }
     } catch (error) {
-        console.error('Error:', error);
-        alert('An error occurred');
+        console.error("Error:", error);
+        alert("An error occurred");
     }
 }
 
 async function autoManual() {
     try {
-        const response = await fetch('http://127.0.0.1:8000/auto-manual', { method: 'GET' });
+        const response = await fetch("http://127.0.0.1:8000/auto-manual", {
+            method: "GET",
+        });
         if (response.ok) {
-            alert('Action executed successfully');
+            alert("Action executed successfully");
             updateTemperature();
         } else {
-            throw new Error('Failed to execute action');
+            throw new Error("Failed to execute action");
         }
     } catch (error) {
-        console.error('Error:', error);
-        alert('An error occurred');
+        console.error("Error:", error);
+        alert("An error occurred");
     }
 }
 
-document.getElementById('actionButton').addEventListener('click', forceCollect);
-document.getElementById('autoButton').addEventListener('click', autoManual);
+document.getElementById("actionButton").addEventListener("click", forceCollect);
+document.getElementById("autoButton").addEventListener("click", autoManual);
 setInterval(updateTemperature, 5000);
 // Initial update
 updateTemperature();
