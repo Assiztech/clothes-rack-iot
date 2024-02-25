e117bf645208900bf8bed71e42252ec7d3a8007a
kanyok <134153117+nullvoid-ky@users.noreply.github.com>
2024-02-25 23:16:36 +0700

    Revert "Feat/kanyok : style grid"

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ async function updateTemperature() {
 
     const statusElement = document.getElementById("is_hanging");
     statusElement.textContent = "Loading...";
-    statusElement.style.cssText = "color: pink;";
     if (data.is_hanging == 1) {
         statusElement.textContent = `Status : Hanging`;
     } else {
@@ -28,7 +27,6 @@ async function updateTemperature() {
     }
 
     const autoButton = document.getElementById("autoButton");
-
     if (data.is_auto) {
         autoButton.textContent = "System: Auto";
     } else {
@@ -37,28 +35,19 @@ async function updateTemperature() {
 
     const temperatureElement = document.getElementById("temperature");
     temperatureElement.textContent = "Loading...";
-    temperatureElement.style.cssText = "color: springgreen;";
     temperatureElement.textContent = `${data.temperature}`;
 
     const humidityElement = document.getElementById("humidity");
     humidityElement.textContent = "Loading...";
-    humidityElement.style.cssText = "color: springgreen;";
     humidityElement.textContent = `${data.humidity}`;
 
     const lightElement = document.getElementById("light");
     lightElement.textContent = "Loading...";
-    lightElement.style.cssText = "color: springgreen;";
     lightElement.textContent = `${data.light}`;
 
     const rainElement = document.getElementById("rain");
     rainElement.textContent = "Loading...";
-    let text = "True";
-    rainElement.style.cssText = "color: springgreen;";
-    if (data.rain <= 1000) {
-        text = "False";
-        rainElement.style.cssText = "color: tomato;";
-    }
-    rainElement.textContent = `${text}`;
+    rainElement.textContent = `${data.rain}`;
 }
 async function forceCollect() {
     try {
